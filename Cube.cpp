d54a93874a142ff737cf6a8143a601f869bab849
Artyom <artyom.lazarev.04@yandex.ru>
2024-12-15 17:50:19 +0300

    Bug fixed
    ...with test application

--- Cube.cpp
+++ Cube.cpp
@@ -4,35 +4,35 @@ void Cube::render() {
 
     glBegin(GL_QUADS);
     // Задняя грань
-    glColor3f(1.0f, 0.0f, 0.0f); glVertex3f(-1.0f + x, -1.0f + obj_y, -1.0f + obj_z);
-    glColor3f(0.0f, 1.0f, 0.0f); glVertex3f(1.0f + x, -1.0f + obj_y, -1.0f + obj_z);
-    glColor3f(0.0f, 0.0f, 1.0f); glVertex3f(1.0f + x, 1.0f + obj_y, -1.0f + obj_z);
-    glColor3f(1.0f, 1.0f, 0.0f); glVertex3f(-1.0f + x, 1.0f + obj_y, -1.0f + obj_z);
+    glColor3f(1.0f, 0.0f, 0.0f); glVertex3f(-1.0f + x, -1.0f + y, -1.0f + z);
+    glColor3f(0.0f, 1.0f, 0.0f); glVertex3f(1.0f + x, -1.0f + y, -1.0f + z);
+    glColor3f(0.0f, 0.0f, 1.0f); glVertex3f(1.0f + x, 1.0f + y, -1.0f + z);
+    glColor3f(1.0f, 1.0f, 0.0f); glVertex3f(-1.0f + x, 1.0f + y, -1.0f + z);
     // Передняя грань
-    glColor3f(1.0f, 0.0f, 1.0f); glVertex3f(-1.0f + x, -1.0f + obj_y, 1.0f + obj_z);
-    glColor3f(0.0f, 1.0f, 1.0f); glVertex3f(1.0f +x , -1.0f + obj_y, 1.0f + obj_z);
-    glColor3f(1.0f, 1.0f, 1.0f); glVertex3f(1.0f + x, 1.0f + obj_y, 1.0f + obj_z);
-    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(-1.0f + x, 1.0f + obj_y, 1.0f + obj_z);
+    glColor3f(1.0f, 0.0f, 1.0f); glVertex3f(-1.0f + x, -1.0f + y, 1.0f + z);
+    glColor3f(0.0f, 1.0f, 1.0f); glVertex3f(1.0f +x , -1.0f + y, 1.0f + z);
+    glColor3f(1.0f, 1.0f, 1.0f); glVertex3f(1.0f + x, 1.0f + y, 1.0f + z);
+    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(-1.0f + x, 1.0f + y, 1.0f + z);
     // Левые грань
-    glColor3f(1.0f, 0.5f, 0.0f); glVertex3f(-1.0f + x, -1.0f + obj_y, -1.0f + obj_z);
-    glColor3f(0.0f, 1.0f, 0.5f); glVertex3f(-1.0f + x, -1.0f + obj_y, 1.0f + obj_z);
-    glColor3f(0.0f, 0.5f, 1.0f); glVertex3f(-1.0f + x, 1.0f + obj_y, 1.0f + obj_z);
-    glColor3f(1.0f, 0.0f, 0.5f); glVertex3f(-1.0f + x, 1.0f + obj_y, -1.0f + obj_z);
+    glColor3f(1.0f, 0.5f, 0.0f); glVertex3f(-1.0f + x, -1.0f + y, -1.0f + z);
+    glColor3f(0.0f, 1.0f, 0.5f); glVertex3f(-1.0f + x, -1.0f + y, 1.0f + z);
+    glColor3f(0.0f, 0.5f, 1.0f); glVertex3f(-1.0f + x, 1.0f + y, 1.0f + z);
+    glColor3f(1.0f, 0.0f, 0.5f); glVertex3f(-1.0f + x, 1.0f + y, -1.0f + z);
     // Правые грань
-    glColor3f(0.5f, 1.0f, 0.0f); glVertex3f(1.0f +x , -1.0f + obj_y, -1.0f + obj_z);
-    glColor3f(0.0f, 1.0f, 0.5f); glVertex3f(1.0f +x , -1.0f + obj_y, 1.0f + obj_z);
-    glColor3f(0.0f, 0.5f, 1.0f); glVertex3f(1.0f +x , 1.0f + obj_y, 1.0f + obj_z);
-    glColor3f(1.0f, 0.0f, 0.5f); glVertex3f(1.0f +x , 1.0f + obj_y, -1.0f + obj_z);
+    glColor3f(0.5f, 1.0f, 0.0f); glVertex3f(1.0f +x , -1.0f + y, -1.0f + z);
+    glColor3f(0.0f, 1.0f, 0.5f); glVertex3f(1.0f +x , -1.0f + y, 1.0f + z);
+    glColor3f(0.0f, 0.5f, 1.0f); glVertex3f(1.0f +x , 1.0f + y, 1.0f + z);
+    glColor3f(1.0f, 0.0f, 0.5f); glVertex3f(1.0f +x , 1.0f + y, -1.0f + z);
     // Верхняя грань
-    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(-1.0f +x , 1.0f + obj_y, -1.0f + obj_z);
-    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(1.0f +x , 1.0f + obj_y, -1.0f + obj_z);
-    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(1.0f +x , 1.0f + obj_y, 1.0f + obj_z);
-    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(-1.0f +x , 1.0f + obj_y, 1.0f + obj_z);
+    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(-1.0f +x , 1.0f + y, -1.0f + z);
+    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(1.0f +x , 1.0f + y, -1.0f + z);
+    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(1.0f +x , 1.0f + y, 1.0f + z);
+    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(-1.0f +x , 1.0f + y, 1.0f + z);
     // Нижняя грань
-    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(-1.0f + x, -1.0f + obj_y, -1.0f + obj_z);
-    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(1.0f + x, -1.0f + obj_y, -1.0f + obj_z);
-    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(1.0f +x , -1.0f + obj_y, 1.0f + obj_z);
-    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(-1.0f + x, -1.0f + obj_y, 1.0f + obj_z);
+    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(-1.0f + x, -1.0f + y, -1.0f + z);
+    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(1.0f + x, -1.0f + y, -1.0f + z);
+    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(1.0f +x , -1.0f + y, 1.0f + z);
+    glColor3f(0.5f, 0.5f, 0.5f); glVertex3f(-1.0f + x, -1.0f + y, 1.0f + z);
     glEnd();
 
 }
